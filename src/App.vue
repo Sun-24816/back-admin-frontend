d42f65b9198ef353c2aaba06a5497caf2ecd3e99
chengyang05 <chengyang05@kuaishou.com>
2024-09-30 17:41:04 +0800

    feat: 完善页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
-  <router-view></router-view>
+  <el-container style="height: 100%; display: flex">
+    <el-aside :width="asideWidth + 'px'" class="aside">
+      <Menu :isCollapse="isCollapse"></Menu>
+    </el-aside>
+    <el-container style="flex: 1">
+      <el-header>
+        <div class="top-nav">
+          <el-icon size="30" @click="handleCollapseMenu">
+            <component :is="isCollapse ? 'Fold' : 'Expand'" />
+          </el-icon>
+        </div>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+<script setup>
+import Menu from "./components/menu.vue";
+import { ref } from "vue";
 
-<style></style>
+const isCollapse = ref(false);
+const asideWidth = ref(200);
+
+const handleCollapseMenu = () => {
+  isCollapse.value = !isCollapse.value;
+  asideWidth.value = isCollapse.value ? 63 : 200;
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+.aside {
+  transition: all 0.5s linear;
+}
+</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,74 +1,57 @@
 <template>
-  <el-container>
-    <el-aside class="side" width="20%">
-      <el-scrollbar>
-        <el-menu>
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon class="icon"><message /></el-icon>
-              <span class="item">系统总览</span>
-            </template>
-            <el-menu-item index="1-1">Option 1</el-menu-item>
-            <el-menu-item index="1-2">Option 2</el-menu-item>
-            <el-menu-item index="1-3">Option 3</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
-              <el-icon class="icon"><Setting /></el-icon>
-              <span class="item">系统管理</span>
-            </template>
-            <el-menu-item index="2-1">用户管理</el-menu-item>
-            <el-menu-item index="2-2">部门管理</el-menu-item>
-            <el-menu-item index="2-3">菜单管理</el-menu-item>
-            <el-menu-item index="2-4">角色管理</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon class="icon"><Shop /></el-icon>
-              <span class="item">商品中心</span>
-            </template>
-            <el-menu-item index="3-1">衣服</el-menu-item>
-            <el-menu-item index="3-2">裤子</el-menu-item>
-            <el-menu-item index="3-3">杯子</el-menu-item>
-            <el-menu-item index="3-4">鞋子</el-menu-item>
-          </el-sub-menu>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
-    <el-container>
-      <el-header>
-        <div>
-          <el-avatar class="avater" :icon="UserFilled"></el-avatar>
-        </div>
-        <el-button type="primary" class="search" :icon="Search">搜索</el-button></el-header
-      >
-      <el-main>
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column type="selection" width="55" />
-          <el-table-column label="序号" width="120">
-            <template #default="scope">{{ scope.row.userId }}</template>
-          </el-table-column>
-          <el-table-column property="name" label="用户名" width="120" />
-          <el-table-column property="address" label="居住地址" />
-          <el-table-column property="telephone" label="手机号码" />
-          <el-table-column property="createTime" label="创建时间" />
-          <el-table-column property="updateTime" label="更新时间" />
-          <el-table-column label="操作">
-            <el-icon><EditPen /></el-icon> <span class="edit">编辑</span>
-            <el-icon><Delete /></el-icon>
-            <span class="delete">删除</span></el-table-column
-          >
-        </el-table>
-      </el-main>
-    </el-container>
-  </el-container>
+  <div class="dashboard">
+    <el-form>
+      <el-row :gutter="20">
+        <el-col v-for="item in columns" :key="item.title" :span="6">
+          <el-form-item :label="item.title + ':'" label-width="100px">
+            <el-input v-model="selectForms[item.formItem]" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <div class="table">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column type="selection" width="55" show-overflow-tooltip />
+        <el-table-column label="序号" width="120" show-overflow-tooltip>
+          <template #default="scope">{{ scope.row.userId }}</template>
+        </el-table-column>
+        <el-table-column property="name" label="用户名" width="120" show-overflow-tooltip />
+        <el-table-column property="address" label="居住地址" show-overflow-tooltip />
+        <el-table-column property="telephone" label="手机号码" show-overflow-tooltip />
+        <el-table-column property="createTime" label="创建时间" show-overflow-tooltip />
+        <el-table-column property="updateTime" label="更新时间" show-overflow-tooltip />
+        <el-table-column label="操作" show-overflow-tooltip>
+          <el-icon><EditPen /></el-icon> <span class="edit">编辑</span>
+          <el-icon><Delete /></el-icon>
+          <span class="delete">删除</span></el-table-column
+        >
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { Menu as IconMenu, House, Delete, EditPen, Shop, Message, Setting } from "@element-plus/icons-vue";
-import { UserFilled } from "@element-plus/icons-vue";
-console.log("abc");
-const tableData = [
+import { ref, watchEffect } from "vue";
+
+const selectForms = ref({
+  userId: undefined,
+  name: "",
+  address: "",
+  telephone: "",
+  createTime: "",
+  updateTime: "",
+});
+
+const columns = [
+  { title: "序号", formItem: "userId" },
+  { title: "用户名", formItem: "name" },
+  { title: "居住地址", formItem: "address" },
+  { title: "手机号码", formItem: "telephone" },
+  { title: "创建时间", formItem: "createTime" },
+  { title: "更新时间", formItem: "updateTime" },
+];
+
+const tableData = ref([
   {
     userId: 1,
     name: "CY YYDS",
@@ -101,63 +84,21 @@ const tableData = [
     createTime: "2022-08-19 13:57:34",
     updateTime: "2022-08-19 13:57:34",
   },
-];
+]);
+
+watchEffect(() => {
+  //筛选表单数据发生改变 就发送网络请求
+  if (selectForms.value.userId) {
+    console.log("abc");
+  }
+});
 </script>
-
 <style scoped>
-.avater {
-  margin-right: 60px;
-  float: right;
-  margin-right: 60px;
+.dashboard {
+  width: 100%;
+  height: 100%;
 }
-
-.edit,
-.delete {
-  color: blue;
-}
-
-.search {
-  margin-right: 30px;
+.table {
   margin-top: 60px;
-  float: right;
-}
-
-.el-header {
-  height: 15%;
-}
-
-.el-aside {
-  height: 100vh;
-  color: white;
-  background-color: #112130;
-}
-
-.el-menu-item,
-.el-sub-menu,
-el-menu {
-  color: white;
-  background-color: #112130;
-}
-
-.item {
-  color: white;
-}
-
-.el-menu-item:hover {
-  background-color: #2961b4;
-}
-
-.icon {
-  color: white;
-}
-
-.el-sub-menu {
-  color: red;
-}
-
-.el-sub-menu__title {
-  align-items: center;
-  box-sizing: border-box;
-  color: white;
 }
 </style>
